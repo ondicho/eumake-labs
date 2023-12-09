a4bc041424c50b69e05718cbc8811187f3a848d4
Ondicho albert <ondicho53@gmail.com>
2023-12-09 04:29:39 +0300

    fix nav

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,11 +1,10 @@
+// Navbar.js
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../../assets/css/navbar.css';
-import logo from '../../assets/images/logo.png'
-import menu from '../../assets/images/menu.png'
-
+import logo from '../../assets/images/logo.png';
+import menu from '../../assets/images/menu.png';
 import call from '../../assets/images/call.png';
-// import callWhite from '../../assets/images/call-white.png';
 
 const Navbar = () => {
   const [isMobile, setIsMobile] = useState(false);
@@ -14,26 +13,49 @@ const Navbar = () => {
     setIsMobile(!isMobile);
   };
 
+  const handleClose = () => {
+    setIsMobile(false);
+  };
+
   return (
-    <nav className="navbar">
-       <div className="mobile-icon">
-      <img className='menu-icon' src={menu} alt='call' />
+    <nav className={`navbar ${isMobile ? 'mobile-view' : ''}`}>
+      <div className="mobile-icon" onClick={handleToggle}>
+        <img className="menu-icon" src={menu} alt="call" />
       </div>
-      <Link to="/" className="logo"><img className="logo" src={logo} alt="logo" /><p>Eumake Diagnostic Laboratories</p></Link>
+      <Link to="/" className="logo">
+        <img className="logo" src={logo} alt="logo" />
+        <p>Eumake Diagnostic Laboratories</p>
+      </Link>
       <div className={`menu ${isMobile ? 'active' : ''}`}>
-        <Link to="/">Home</Link>
-        <Link to="/services">Services</Link>
-        <Link to="/about-us">About</Link>
-        <Link to="/faqs">FAQs</Link>
-        <Link to="/contact-us">Contact</Link>
-        <Link to="/contact-us"><img className='call' src={call} alt='call' /></Link>
+        <Link to="/" onClick={handleClose}>Home</Link>
+        <Link to="/services" onClick={handleClose}>Services</Link>
+        <Link to="/about-us" onClick={handleClose}>About</Link>
+        <Link to="/faqs" onClick={handleClose}>FAQs</Link>
+        <Link to="/contact-us" onClick={handleClose}>Contact</Link>
+        <Link to="/contact-us" onClick={handleClose}><img className='call' src={call} alt='call' /></Link>
       </div>
       <div className="burger-menu" onClick={handleToggle}>
         <div className="bar1"></div>
         <div className="bar2"></div>
         <div className="bar3"></div>
       </div>
-     
+      {isMobile && (
+        <div className="mobile-menu" onClick={handleClose}>
+          <div className="mobile-menu-content">
+            <Link to="/" className="logo">
+              <img className="logo" src={logo} alt="logo" />
+              <p>Eumake Diagnostic Laboratories</p>
+            </Link>
+            <div className="menu">
+              <Link to="/" onClick={handleClose}>Home</Link>
+              <Link to="/services" onClick={handleClose}>Services</Link>
+              <Link to="/about-us" onClick={handleClose}>About</Link>
+              <Link to="/faqs" onClick={handleClose}>FAQs</Link>
+              <Link to="/contact-us" onClick={handleClose}>Contact</Link>
+            </div>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
