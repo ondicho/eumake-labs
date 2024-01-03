6f891919342d84af37730cfac14cce4cea17a283
Ondicho albert <ondicho53@gmail.com>
2024-01-03 11:49:49 +0300

    director

--- src/components/pages/Team.jsx
+++ src/components/pages/Team.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Card from './Card'; // Import the Card component
 import '../../assets/css/team.css';
-import image1 from '../../assets/images/_E5A0965.jpg';
+import image1 from '../../assets/images/_E5A0993.jpg';
 import image2 from '../../assets/images/_E5A0794.jpg';
 import image3 from '../../assets/images/_E5A0792.jpg';
 
